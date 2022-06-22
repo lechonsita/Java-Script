c0f4b240cf8d1dc75c544741a09331c0c920f2f2
lechonsita <104104031+lechonsita@users.noreply.github.com>
2022-06-22 14:42:33 +0300

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,10 @@
+<script>
+	$( document ).ready(function(){
+	  $( ".out" ).click(function(){ 
+	    $( "div" ).fadeOut(); 
+	  });	
+      $( ".in" ).click(function(){ 
+	    $( "div" ).fadeIn(); 
+	  });
+    });
+		</script>
